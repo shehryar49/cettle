c20d19477f3ba8373c67d8ea7bcad30a026a1c57
shehryar49 <shehryarshahid49@gmail.com>
2023-09-23 13:37:06 +0500

    Did something

--- backend/cettle.py
+++ backend/cettle.py
@@ -2,12 +2,21 @@ from flask import Flask,request,jsonify
 import json
 
 app = Flask("cettle")
+
 # Load data
+
 courses = json.loads(open("courses.json").read())
+venues = json.loads(open("venues.json").read())
+
+
 # Callbacks
-@app.route("/courses",methods = ["GET"])
+
+@app.route("/courses")
 def listCourses():
     return jsonify(courses),200
 
+@app.route("/venues")
+def listVenues():
+    return jsonify(venues),200
 ##
 app.run()
