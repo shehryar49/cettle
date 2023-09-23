a4aa10c9385dd6f7ea8782db8867675be4149661
Ahmad-Waseem <eagle24623@gmail.com>
2023-09-23 19:41:22 +0500

    Semi-Final Copyable FrontEnd
    Redesigned w.r.t UML
    Solved glitch in courses buttons
    Made a Saucy interface
    Add some Pasting Feasabilty Feature
    Now, just have to copy and change Variable names for Other pages
    
    Calander.js left; to be disscussed
    Nav bar button error *Remains*
    TimeTable.html to be disscussed
    
    **Other pages may be completed until tonight

---
+++ frontend/cettle/courses.js
@@ -0,0 +1,56 @@
+// custom.js
+
+document.getElementById('courseCodeInput')
+.addEventListener('paste', function(e)
+{
+    e.preventDefault(); // stpo default pasting
+
+    // Get Paste string
+    const pastedText = (e.clipboardData || window.clipboardData).getData('text');
+    // Split the pasted content by spaces
+    const halfstring = pastedText.split(' ');
+    // give halfstring to Code and Course Name
+    if (halfstring.length >= 1)
+    {
+        document.getElementById('courseCodeInput').value = halfstring[0];
+    }
+    if (halfstring.length >= 2)
+    {
+        document.getElementById('courseNameInput').value = halfstring.slice(1).join(' ');
+    }
+});
+
+$(document).ready(function() 
+{
+    $('#courseAddButton').click(function() 
+    {
+        toggleCollapse('#inputFields');
+    });
+
+    // Update
+    $('#courseUpdateButton').click(function() 
+    {
+        toggleCollapse('#updateFields');
+    });
+
+    //Delete Course
+    $('#courseDeleteButton').click(function() 
+    {
+        toggleCollapse('#deleteFields');
+    });
+
+    function toggleCollapse(target) 
+    {                                       // if target button is open then close
+        var $target = $(target);            // else, close all and then open it
+        if ($target.hasClass('show')) 
+        {
+            $target.collapse('hide');
+        } 
+        else 
+        {
+            $('.collapse').collapse('hide');    // Try to Remove delay in FINAL VERSION
+            $target.collapse('show');           // ^Done and it looks Cooler than expected
+        }
+    }
+});
+
